87e422be747e4d3c76372ce87e97db6750a66d97
Chalida (Anita) Ruangrotsakun <ruangroc@oregonstate.edu>
2023-02-28 23:33:48 -0800

    setup basic unit testing framework using vitest

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+import {defineConfig} from 'vitest/config';
+
+module.exports = defineConfig(async ({ command, mode }) => {
+    const { svelte } = await import('@sveltejs/vite-plugin-svelte');
+    return {
+      plugins: [svelte()],
+      test: {
+        include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+        globals: true,
+        environment: 'jsdom',
+      },
+  }
+});
