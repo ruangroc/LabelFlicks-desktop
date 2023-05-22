ef7318df278fa58d3267a0f9b8b78cb84e777c8d
Chalida (Anita) Ruangrotsakun <ruangroc@oregonstate.edu>
2023-05-21 20:07:46 -0700

    more responsive design for frames control buttons

--- src/renderer/src/routes/label/Label.svelte
+++ src/renderer/src/routes/label/Label.svelte
@@ -3,14 +3,19 @@
     import { projectVideos } from "../../stores/videos";
     import { videoFrames, currentBoxes, projectLabels, fetchVideoFrames, fetchBoundingBoxes, fetchLabels } from "../../stores/labeling";
     import { onMount } from "svelte";
+    import { Stretch } from 'svelte-loading-spinners';
 
+    let showLoadingSymbol = true;
     let selectedVideoID = $projectVideos[0].id;
     let selectedFrame = "";
+    let frameIndex = 0;
+    let defaultStyle = "border-solid border-2 border-gray-400 mx-1 p-1 rounded text-sm";
 
     onMount(async () => {
         await fetchLabels($selectedProject.id);
         await fetchVideoFrames(selectedVideoID);
-        selectedFrame = $videoFrames[0];
+        selectedFrame = $videoFrames[frameIndex];
+        showLoadingSymbol = false;
     });
 
     function getPercentFramesLabeled() {
@@ -22,10 +27,15 @@
         let percent = 100 * (numLabeled / $videoFrames.length);
         return percent;
     }
+
+    function changeFrame(value) {
+        frameIndex += value;
+        selectedFrame = $videoFrames[frameIndex];
+    }
 </script>
 
-<div class="flex flex-col items-center w-5/6 mx-auto mt-2">
-    <div class="flex flex-row w-full justify-end mb-1">
+<div class="flex flex-col items-center w-5/6 mx-auto">
+    <div class="flex flex-row w-full justify-end my-2">
         <button class="bg-gray-300 ml-auto p-2 rounded"
             ><a href="/">Save</a></button
         >
@@ -54,87 +64,90 @@
         </div>
     </div>
 
-    <div class="flex flex-row w-full justify-start mb-2 py-1">
-        <!-- Display current frame and player controls -->
-        <!-- TODO: make responsive. It's only fine in full screen rn -->
-        <div class="w-1/2 flex flex-col">
-            <img src="{selectedFrame.frame_url}" alt="still frame from the video" />
-            <div class="my-1 flex flex-row">
-                <p class="mr-6">
-                    Frame: {selectedFrame ? selectedFrame.frame_url.split("/").at(-1) : ""}
-                </p>
-                <button class="border-solid border-2 border-gray-400 mr-1 p-1 rounded text-sm">
-                    Speed
-                </button>
-                <button class="border-solid border-2 border-gray-400 ml-1 p-1 rounded text-sm">
-                    Play/Pause
-                </button>
-                <button class="border-solid border-2 border-gray-400 mx-1 p-1 rounded text-sm">
-                    Back
-                </button>
-                <button class="border-solid border-2 border-gray-400 mr-6 p-1 rounded text-sm">
-                    Next
-                </button>
-                <button class="border-solid border-2 border-gray-400 mr-1 p-1 rounded text-sm">
-                    Add Bounding Box
-                </button>
-                <button class="border-solid border-2 border-gray-400 mr-1 p-1 rounded text-sm">
-                    Finetune Predictions
-                </button>
-            </div>
-        </div>
-        
-        <!-- Display labels -->
-        <div class="w-1/2 pl-4">
-            <div class="flex flex-row">
-                <h2 class="text-left text-lg font-semibold my-1">Object Detection Labels</h2>
-                <button class="border-solid border-2 border-gray-400 ml-auto p-1 rounded text-sm">
-                    Add Label
-                </button>
-            </div>
-            
+    {#if showLoadingSymbol}
+        <Stretch size="60" color="#FF3E00" unit="px" duration="1s" />
+    {:else}
+        <div class="grid grid-cols-2">
+            <!-- Display current frame and player controls -->
             <div class="flex flex-col">
-                {#each $projectLabels as label}
-                    <div class="flex flex-row">
-                        <p class="mr-4">{label.name}</p>
-                        <button class="border-solid border-2 border-gray-400 mx-1 p-1 rounded text-sm">
-                            Rename
+                <img class="h-3/4" src="{selectedFrame.frame_url}" alt="still frame from the video" />
+
+                <div class="my-1 grid grid-cols-4">
+
+                    <div class="col-span-1 mx-auto">
+                        <p class="mr-6">
+                            Frame: {selectedFrame ? selectedFrame.frame_url.split("/").at(-1) : ""}
+                        </p>
+                    </div>
+                    
+                    <div class="col-span-2 mx-auto">
+                        <button 
+                            class={`${frameIndex === 0 ? "disabled" : ""} ${defaultStyle}`} 
+                            on:click={() => changeFrame(-1)}
+                        >
+                            Back
                         </button>
-                        <button class="border-solid border-2 border-gray-400 mx-1 p-1 rounded text-sm">
-                            Color
+
+                        <button class="border-solid border-2 border-gray-400 ml-1 p-1 rounded text-sm">
+                            Play/Pause
                         </button>
-                        <button class="border-solid border-2 border-gray-400 mx-1 p-1 rounded text-sm">
-                            Delete
+
+                        <button 
+                            class={`${frameIndex === $videoFrames.length-1 ? "disabled" : ""} ${defaultStyle}`} 
+                            on:click={() => changeFrame(1)}
+                        >
+                            Next
                         </button>
                     </div>
-                    <div class="w-full h-4 bg-gray-300 mt-1 mb-4"></div>
+
+                    <div class="col-span-1 mx-auto">
+                        <button class="border-solid border-2 border-gray-400 mr-1 p-1 rounded text-sm">
+                            Add Bounding Box
+                        </button>
+                        <button class="border-solid border-2 border-gray-400 mr-1 p-1 rounded text-sm">
+                            Finetune Predictions
+                        </button>
+                    </div>
                     
-                {/each}
+                </div>
+            </div>
+            
+            <!-- Display labels -->
+            <div class="pl-4 overflow-y-auto">
+                <div class="flex flex-row">
+                    <h2 class="text-left text-lg font-semibold my-1">Object Detection Labels</h2>
+                    <button class="border-solid border-2 border-gray-400 ml-auto p-1 rounded text-sm">
+                        Add Label
+                    </button>
+                </div>
+                
+                <div class="flex flex-col">
+                    {#each $projectLabels as label}
+                        <div class="flex flex-row">
+                            <p class="mr-4">{label.name}</p>
+                            <button class="border-solid border-2 border-gray-400 mx-1 p-1 rounded text-sm">
+                                Rename
+                            </button>
+                            <button class="border-solid border-2 border-gray-400 mx-1 p-1 rounded text-sm">
+                                Color
+                            </button>
+                            <button class="border-solid border-2 border-gray-400 mx-1 p-1 rounded text-sm">
+                                Delete
+                            </button>
+                        </div>
+                        <!-- TODO: add colored segments where labels are detected -->
+                        <div class="w-full h-4 bg-gray-300 mt-1 mb-4"></div>
+                    {/each}
+                </div>
             </div>
         </div>
-    </div>
-    
-
-    
-    <!-- Experimentation below -->
-
-    <!-- <label for="select-frame"><h2>Select frame:</h2></label>
-    <select name="select-frame" bind:value={selectedFrame.id} on:change="{fetchBoundingBoxes(selectedFrame.id)}">
-        {#each $videoFrames as frame}
-            <option value="{frame.id}">{frame.id}</option>
-        {/each}
-    </select>
-
-    <p>Number of boxes: {$currentBoxes.length}</p>
-
-    <label for="select-label"><h2>Select label:</h2></label>
-    <select name="select-label" bind:value={selectedLabelID}>
-        {#each $projectLabels as label}
-            <option value="{label.id}">{label.name}</option>
-        {/each}
-    </select>
-
-    {#each $videoFrames as frame}
-        <img src="{frame.frame_url}" alt="still frame from the video" />
-    {/each} -->
+    {/if}
 </div>
+
+<style>
+	.disabled {
+		pointer-events: none;
+		cursor: not-allowed;
+		color: darkgray;
+	}
+</style>
