834b93efc04fa94c9fd9bcf8a749c81929def3f7
Chalida (Anita) Ruangrotsakun <ruangroc@oregonstate.edu>
2023-08-04 22:40:28 -0700

    added tests for labeling stores to increase code coverage

--- src/renderer/src/tests/Stores.test.js
+++ src/renderer/src/tests/Stores.test.js
@@ -109,44 +109,40 @@ const mockVideos = [
       }
 ];
 
-const expectedVideosTableData = [
+const mockFrames = [
     {
-        "Name": "cats-1.mp4",
-        "Date Uploaded": "2023-05-16",
-        "Percent Labeled": "10%",
-        "Number of Frames": "25"
+      "human_reviewed": false,
+      "width": 640,
+      "height": 480,
+      "project_id": "project-id-3",
+      "video_id": "video-id-10",
+      "frame_url": "images/0.jpg",
+      "id": "frame-id-100",
+      "labels": []
     },
     {
-        "Name": "cats-2.mp4",
-        "Date Uploaded": "2023-05-16",
-        "Percent Labeled": "50%",
-        "Number of Frames": "20"
-    },
-];
-
-const expectedVideosExportTableData = [
-    {
-        "Name": "cats-1.mp4",
-        "Percent Labeled": "10%",
-        "Number of Frames": "25"
+      "human_reviewed": false,
+      "width": 640,
+      "height": 480,
+      "project_id": "project-id-3",
+      "video_id": "video-id-10",
+      "frame_url": "images/1.jpg",
+      "id": "frame-id-101",
+      "labels": [
+        "label-id-10"
+      ]
     },
     {
-        "Name": "cats-2.mp4",
-        "Percent Labeled": "50%",
-        "Number of Frames": "20"
-    },
-];
-
-const expectedPreprocessingTableData = [
-    {
-        "Name": "cats-1.mp4",
-        "Date Uploaded": "2023-05-16",
-        "Status": 1
-    },
-    {
-        "Name": "cats-2.mp4",
-        "Date Uploaded": "2023-05-16",
-        "Status": 2
+      "human_reviewed": false,
+      "width": 640,
+      "height": 480,
+      "project_id": "project-id-3",
+      "video_id": "video-id-10",
+      "frame_url": "images/2.jpg",
+      "id": "frame-id-102",
+      "labels": [
+          "label-id-10"
+      ]
     },
 ];
 
@@ -173,6 +169,61 @@ const server = setupServer(
             "project_id": "project-id-1",
             "videos": mockVideos,
         }));
+    }),
+    rest.get(`http://localhost:${server_port}/projects/project-id-2/labels`, (req, res, ctx) => {
+        return res(ctx.json({
+            "project_id": "project-id-2",
+            "labels": [
+                {
+                  "name": "Siamese",
+                  "project_id": "project-id-2",
+                  "id": "label-id-1"
+                },
+                {
+                  "name": "Tabby",
+                  "project_id": "project-id-2",
+                  "id": "label-id-2"
+                },
+                {
+                  "name": "Persian Blue",
+                  "project_id": "project-id-2",
+                  "id": "label-id-3"
+                },
+            ]
+          }));
+    }),
+    rest.get(`http://localhost:${server_port}/videos/video-id-10/frames`, (req, res, ctx) => {
+        return res(ctx.json({
+            "video_id": "video-id-10",
+            "frames": mockFrames
+        }));
+    }),
+    rest.get(`http://localhost:${server_port}/frames/frame-id-102/inferences`, (req, res, ctx) => {
+        return res(ctx.json({
+            "frame_id": "frame-id-102",
+            "bounding_boxes": [
+              {
+                "x_top_left": 107,
+                "y_top_left": 150,
+                "x_bottom_right": 148,
+                "y_bottom_right": 208,
+                "width": 40,
+                "height": 57,
+                "frame_id": "frame-id-102",
+                "label_id": "label-id-10"
+              },
+              {
+                "x_top_left": 268,
+                "y_top_left": 26,
+                "x_bottom_right": 286,
+                "y_bottom_right": 83,
+                "width": 18,
+                "height": 57,
+                "frame_id": "frame-id-102",
+                "label_id": "label-id-10"
+              },
+            ]
+          }));
     }),
 );
   
@@ -217,6 +268,47 @@ test('downloadAnnotations will return annotations path', async () => {
 });
 
 test('fetchVideos will populate projectVideos and videosTableData and videosExportTableData and ', async () => {
+    const expectedVideosTableData = [
+        {
+            "Name": "cats-1.mp4",
+            "Date Uploaded": "2023-05-16",
+            "Percent Labeled": "10%",
+            "Number of Frames": "25"
+        },
+        {
+            "Name": "cats-2.mp4",
+            "Date Uploaded": "2023-05-16",
+            "Percent Labeled": "50%",
+            "Number of Frames": "20"
+        },
+    ];
+    
+    const expectedVideosExportTableData = [
+        {
+            "Name": "cats-1.mp4",
+            "Percent Labeled": "10%",
+            "Number of Frames": "25"
+        },
+        {
+            "Name": "cats-2.mp4",
+            "Percent Labeled": "50%",
+            "Number of Frames": "20"
+        },
+    ];
+    
+    const expectedPreprocessingTableData = [
+        {
+            "Name": "cats-1.mp4",
+            "Date Uploaded": "2023-05-16",
+            "Status": 1
+        },
+        {
+            "Name": "cats-2.mp4",
+            "Date Uploaded": "2023-05-16",
+            "Status": 2
+        },
+    ];
+
     expect(get(videos.projectVideos)).toEqual([]);
     expect(get(videos.videosTableData)).toEqual([]);
     expect(get(videos.videosExportTableData)).toEqual([]);
@@ -228,3 +320,84 @@ test('fetchVideos will populate projectVideos and videosTableData and videosExpo
     expect(get(videos.preprocessingTableData)).toEqual(expectedPreprocessingTableData);
 });
 
+test('fetchLabels should populate projectLabels and create the mappings between labels IDs and names', async () => {
+    const mockLabels = [
+        {
+          "name": "Siamese",
+          "project_id": "project-id-2",
+          "id": "label-id-1",
+          "hidden": false,
+        },
+        {
+          "name": "Tabby",
+          "project_id": "project-id-2",
+          "id": "label-id-2",
+          "hidden": false,
+        },
+        {
+          "name": "Persian Blue",
+          "project_id": "project-id-2",
+          "id": "label-id-3",
+          "hidden": false,
+        },
+    ];
+
+    const expectedLabelIdToNameMapping = {
+        "label-id-1": "Siamese",
+        "label-id-2": "Tabby",
+        "label-id-3": "Persian Blue"
+    };
+
+    const expectedNameToLabelIdMapping = {
+        "Siamese": "label-id-1",
+        "Tabby": "label-id-2",
+        "Persian Blue": "label-id-3",
+    };
+
+    expect(get(labeling.projectLabels)).toEqual([]);
+    expect(get(labeling.labelIdToName)).toEqual({});
+    expect(get(labeling.nameToLabelId)).toEqual({});
+    await labeling.fetchLabels("project-id-2");
+    expect(get(labeling.projectLabels)).toEqual(mockLabels);
+    expect(get(labeling.labelIdToName)).toEqual(expectedLabelIdToNameMapping);
+    expect(get(labeling.nameToLabelId)).toEqual(expectedNameToLabelIdMapping);
+});
+
+test('fetchVideoFrames sets videoFrames', async () => {
+    expect(get(labeling.videoFrames)).toEqual([]);
+    await labeling.fetchVideoFrames("video-id-10");
+    expect(get(labeling.videoFrames)).toEqual(mockFrames);
+});
+
+test('fetchBoundingBoxes sets currentBoxes', async () => {
+    const boxesWithAddedFields = [
+        {
+            "x_top_left": 107,
+            "y_top_left": 150,
+            "x_bottom_right": 148,
+            "y_bottom_right": 208,
+            "width": 40,
+            "height": 57,
+            "frame_id": "frame-id-102",
+            "label_id": "label-id-10",
+            "edited": false, 
+            "checked": true
+          },
+          {
+            "x_top_left": 268,
+            "y_top_left": 26,
+            "x_bottom_right": 286,
+            "y_bottom_right": 83,
+            "width": 18,
+            "height": 57,
+            "frame_id": "frame-id-102",
+            "label_id": "label-id-10",
+            "edited": false, 
+            "checked": true
+          },
+    ];
+
+    expect(get(labeling.currentBoxes)).toEqual([]);
+    await labeling.fetchBoundingBoxes("frame-id-102");
+    expect(get(labeling.currentBoxes)).toEqual(boxesWithAddedFields);
+});
